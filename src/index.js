a8280cf4473a8ed1652457474f9b612f207498f8
tivics <christian.schaefer.07@googlemail.com>
2022-06-13 10:20:48 +0200

    Version 0.3.5

--- src/index.js
+++ src/index.js
@@ -18,33 +18,32 @@ client.on('messageCreate', async (message) => {
             selfDeaf: false,
         })
     }
-    else if(message.content.slice(0, 6) === '!play ') {
-        
-        var url = message.content.slice(6)
-        
-        connection = joinVoiceChannel({
+    else if(message.content.slice(0, 6) === '!play ') {    
+        var url = message.content.slice(6)       
+        var connection = joinVoiceChannel({
             channelId: message.member.voice.channel.id,
             guildId: message.guild.id,
             adapterCreator: message.guild.voiceAdapterCreator,
             selfMute: false,
             selfDeaf: false,
         })      
-
         const stream = ytdl(url, { filter: 'audioonly', quality: 'highestaudio', highWaterMark: 1024 * 1024 * 10 ,liveBuffer: 4000 }) // Buffergröße 10MB für Videos und 4 anstatt 2 Sekunden für Live Streams
         const resource = createAudioResource(stream, { inlineVolume: false/*true*/ }) // TODO better solution for volume control
         //resource.volume.setVolume(0.1)
-
+        //play audio stream
         player.play(resource)
-     
         connection.subscribe(player)    
+
+        //delete message to keep channel clean 
         setTimeout(() => message.delete(), 10000)
         
+        //pause news/twitter handler while playing audio stream
         clearInterval(news_handler)
         clearInterval(twitter_handler)
         news_handler = 0
         twitter_handler = 0
 
-        //error handler, post message and leave channel
+        //error handler, post message, leave channel and reset news/twitter handler interval
         player.on('error', error => {
             console.error(`Error: ${error.message}`);
             setTimeout(() => connection.disconnect(), 10000)
@@ -53,7 +52,7 @@ client.on('messageCreate', async (message) => {
                 twitter_handler = setInterval(function(){news.read_twitter(client)}, 60000)
             }
         })
-        //leave channel once done
+        //leave channel once done and reset news/twitter handler interval
         player.on(AudioPlayerStatus.Idle, () => {
             setTimeout(() => connection.disconnect(), 10000)
             if(news_handler === 0 && twitter_handler === 0){
@@ -69,15 +68,10 @@ client.on('messageCreate', async (message) => {
 })
 
 client.on('ready', async client => {
-
+    //refresh rss feed every minute
     news_handler = setInterval(function(){news.read_rss(client)}, 60000)
-
-    twitter_handler = setInterval(function(){news.read_twitter(client)}, 60000)
-
-    //gaming-news channel
-    //var news_channel = client.channels.cache.find(channel => channel.id === `961721436546949140`)
-    //Hunt-News Channel
-    //var hunt_channel = client.channels.cache.find(channel => channel.id === `961721387754606682`)
+    //refres twitter feed every 15 minutes
+    twitter_handler = setInterval(function(){news.read_twitter(client)}, 900000)
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN)
--- src/news.js
+++ src/news.js
@@ -7,26 +7,16 @@ var parser = new Parser()
 var db = new sqlite3.Database('./news.db');
 
 module.exports = {
-  read_rss: async function (client) {  
-    
+  init: async function(){
+    db.run("INSERT INTO NEWS (ID,PUB_DATE,TITLE,PROVIDER) VALUES (1,'1000-01-01T00:00:00.000Z','INIT','GAMESTAR')")
+    let twitter_userID = await twitterClient.v2.userByUsername(process.env.TWITTER_USER)
+    db.run("INSERT INTO TWEETS (ID,TWEET_ID,USER_ID) VALUES (1,'0000'," + twitter_userID.data.id + ")")
+  },
+  read_rss: async function (client) {     
     //gaming-news channel
     const channel = client.channels.cache.find(channel => channel.id === process.env.CHANNEL_RSS)
     var db_result
-
-    //clear record
-    //db.run("DELETE FROM NEWS WHERE ID = 1;")
-    //initial record
-    //db.run("INSERT INTO NEWS (ID,PUB_DATE,TITLE,PROVIDER) VALUES (1,'1000-01-01T00:00:00.000Z','INIT','GAMESTAR')")
-
-    /* first draft with only 1 record to keep db size small in future --> caused issue with multiple message at the same time
-        //db update
-        //let stmt = db.prepare('UPDATE NEWS SET PUB_DATE = ?, TITLE = ? WHERE PROVIDER = ?;') 
-        //let updates = stmt.run(String(last_pubDate), String(last_title), 'GAMESTAR')
-
-        //output check
-        //console.log(item.title + '\n' + item.contentSnippet + '\n' + item.link)
-    */
-
+    //buffer news db
     db.all("SELECT ID,PUB_DATE,TITLE,PROVIDER FROM NEWS where PROVIDER=$provider" , {
       $provider: 'GAMESTAR',
     },
@@ -37,41 +27,34 @@ module.exports = {
       else{
         db_result = rows
       }
-    }
-    )
+    })
     //request and parse RSS feed 
     let feed = await parser.parseURL(process.env.RSS_FEED)
     //loop through results
     feed.items.forEach(item => {
-          date = new Date(item.pubDate)         
+      date = new Date(item.pubDate)         
 
-          let check = db_result.every(row => {
-            return !(row.TITLE === item.title)
-          })
-
-          if(check != false){
-            //new db entry and post message
-            db.run('INSERT INTO NEWS(ID,PUB_DATE,TITLE,PROVIDER) VALUES((SELECT MAX( ID ) FROM NEWS) +1, ?, ? ,?)', [String(date), String(item.title), 'GAMESTAR'], (err) => {
-              if(err) {
-                return console.log(err.message)
-              }
-              //... TODO console output new db entry
-            })
-            channel.send(item.title + '\n' + item.contentSnippet + '\n' + item.link)
-          }
+      let check = db_result.every(row => {
+        return !(row.TITLE === item.title)
       })
-    }, 
-  read_twitter: async function (client) {
 
+      if(check != false){
+        //new db entry and post message
+        db.run('INSERT INTO NEWS(ID,PUB_DATE,TITLE,PROVIDER) VALUES((SELECT MAX( ID ) FROM NEWS) +1, ?, ? ,?)', [String(date), String(item.title), 'GAMESTAR'], (err) => {
+          if(err) {
+            return console.log(err.message)
+          }
+        })
+        channel.send(item.title + '\n' + item.contentSnippet + '\n' + item.link)
+      }
+    })
+  }, 
+  read_twitter: async function (client) {
     //twitter-news channel
     const channel = client.channels.cache.find(channel => channel.id === process.env.CHANNEL_TWITTER)
-
     const userID = await twitterClient.v2.userByUsername(process.env.TWITTER_USER)  
     var last_tweet
-    
-    //initial record
-    //db.run("INSERT INTO TWEETS (ID,TWEET_ID,USER_ID) VALUES (1,'0000'," + userID.data.id + ")")
-
+    //loop through db entries and get last tweet id
     db.all("SELECT ID,TWEET_ID,USER_ID FROM TWEETS where USER_ID=$userID AND ID=$id" , {
       $userID: userID.data.id,
       $id: 1
@@ -86,16 +69,16 @@ module.exports = {
         })}
       }
     )
-
-    twitterClient.v2.userTimeline(String(userID.data.id), { exclude: 'replies' } ).then ((response) => {
-      response.tweets.forEach(tweet => {
-        if(tweet.id === response.meta.newest_id && tweet.id != last_tweet && tweet.author_id === userID){
+    //get tweets from specific user
+    twitterClient.v2.userTimeline(String(userID.data.id), { exclude: 'replies' }).then ((response) => {
+      response.tweets.forEach(tweet => {      
+        if(tweet.id === response.meta.newest_id && tweet.id != last_tweet){
           //update db and post message to channel
           let stmt = db.prepare('UPDATE TWEETS SET TWEET_ID = ? WHERE USER_ID = ?;') 
-          let updates = stmt.run(String(tweet.id), String(userID.data.id))
+          stmt.run(String(tweet.id), String(userID.data.id))
           channel.send(tweet.text)
         }
-      });
-        }).catch ((err) => console.error(err))
+      })
+    }).catch ((err) => console.error(err))
   }
 }
